b94e4d106701c5fc3d5d972dddb7b6aa6f0c35f4
Theonlysmartboy <o2jose43@gmail.com>
2026-03-10 17:29:40 +0300

    Begun designing product viewer

--- SalesManViewer/Form1.vb
+++ SalesManViewer/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim chk As New DataGridViewCheckBoxColumn()
+        chk.HeaderText = "Select"
+        chk.Width = 50
+        DgvLocalProducts.Columns.Insert(0, chk)
+        DgvLocalProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        DgvLocalProducts.MultiSelect = False
+        DgvLocalProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
 
+        Dim chk1 As New DataGridViewCheckBoxColumn()
+        chk1.HeaderText = "Select"
+        chk1.Width = 50
+        DgvOnlineProducts.Columns.Insert(0, chk1)
+        DgvOnlineProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        DgvOnlineProducts.MultiSelect = False
+        DgvOnlineProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+    End Sub
 End Class
